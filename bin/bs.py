aea6a61320aca3adbb0f8e8a797ffd3eeac7093f
mama <tomas.kulich@gmail.com>
2013-11-21 20:12:07 +0100

    polishing

--- bin/bs.py
+++ bin/bs.py
@@ -1,6 +1,6 @@
 html="""
 <body pokus='true'>
-    <div class='container'>
+    <div class='container', onclick={handler} >
         <div id='class'>Something here</div>
         <div>Something else</div>
     </div>
@@ -10,6 +10,20 @@ html="""
 from bs4 import BeautifulSoup as BS
 from bs4 import NavigableString
 
+def _val(val):
+    if isinstance(val, list):
+        val = val[0]
+    if val[0]=='{' and val[-1]=='}':
+        return val[1:-1]
+    else:
+        return "'"+val+"'"
+
+def _key(key):
+    if key=='class':
+        return 'className'
+    else:
+        return "'"+key+"'"
+
 
 def to_react(node, indent=0):
     indent_str = " "*4*indent
@@ -18,7 +32,7 @@ def to_react(node, indent=0):
              return None
         else:
              return indent_str+'''"%s"'''%str(node).strip(' \n\t')
-    props = ','.join("%s: %s"%(str(key), str(val[0])) for key, val in node.attrs.items())
+    props = ', '.join("%s: %s"%(_key(key), _val(val)) for key, val in node.attrs.items())
     children = ',\n'.join(to_react(child, indent+1) for child in node.children if to_react(child))
     return '%s%s({%s}, [\n%s\n%s])'%(indent_str,node.name,props,children,indent_str)
 
