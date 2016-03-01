61295fac16fc3d962dfd2cbb07bd07e82da0165b
Aaron Lademann <aaron.lademann@workiva.com>
2016-03-01 07:08:17 -0700

    Add / update license comments

--- lib/react_dom.dart
+++ lib/react_dom.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, the Clean project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 library react_dom;
 
 /// Renders a ReactElement into the DOM in the supplied [container] and return a reference to the [component]
