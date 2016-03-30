82e257a2ed5fe86d16399e52441144cb6d60d8f0
Greg Littlefield <greg.littlefield@webfilings.com>
2016-03-30 12:10:20 -0700

    Add doc comments to js_interop_helpers

--- lib/react_client/js_interop_helpers.dart
+++ lib/react_client/js_interop_helpers.dart
@@ -1,3 +1,5 @@
+/// Utilities for reading/modifying dynamic keys on JavaScript objects
+/// and converting Dart [Map]s to JavaScript objects.
 library react_client.js_interop_helpers;
 
 import "package:js/js.dart";
@@ -14,7 +16,11 @@ external dynamic _setProperty(jsObj, String key, value);
 typedef dynamic _GetPropertyFn(jsObj, String key);
 typedef dynamic _SetPropertyFn(jsObj, String key, value);
 
-// Necessary because `external operator[]` isn't allowed on JS interop classes https://github.com/dart-lang/sdk/issues/25053
+/// Returns the property at the given [_GetPropertyFn.key] on the
+/// specified JavaScript object [_GetPropertyFn.jsObj]
+///
+/// Necessary because `external operator[]` isn't allowed on JS interop classes
+/// (see: https://github.com/dart-lang/sdk/issues/25053).
 final _GetPropertyFn getProperty = (() {
   _eval(r'''
     function _getProperty(obj, key) {
@@ -25,7 +31,11 @@ final _GetPropertyFn getProperty = (() {
   return _getProperty;
 })();
 
-// Necessary because `external operator[]=` isn't allowed on JS interop classes https://github.com/dart-lang/sdk/issues/25053
+/// Sets the property at the given [_SetPropertyFn.key] to [_SetPropertyFn.value]
+/// on the specified JavaScript object [_SetPropertyFn.jsObj]
+///
+/// Necessary because `external operator[]=` isn't allowed on JS interop classes
+/// (see: https://github.com/dart-lang/sdk/issues/25053).
 final _SetPropertyFn setProperty = (() {
   _eval(r'''
     function _setProperty(obj, key, value) {
@@ -37,12 +47,19 @@ final _SetPropertyFn setProperty = (() {
 })();
 
 
+/// A interop class for an anonymous JavaScript object, with no properties.
+///
+/// For use when dealing with dynamic properties via [getProperty]/[setProperty].
 @JS()
 @anonymous
 class EmptyObject {
   external factory EmptyObject();
 }
 
+/// Returns [map] converted to a JavaScript object, similar to
+/// `new JsObject.jsify` in `dart:js`.
+///
+/// Recursively converts nested [Map]s, and wraps [Function]s with [allowInterop].
 EmptyObject jsify(Map map) {
   var jsMap = new EmptyObject();
 
