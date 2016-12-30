f93f521ca54e2e37296644829687fccc81eb1476
Greg Littlefield <greg.littlefield@workiva.com>
2016-12-30 01:17:41 -0700

    Add jsifyAndAllowInterop, deprecate jsify

--- lib/react_client/js_interop_helpers.dart
+++ lib/react_client/js_interop_helpers.dart
@@ -91,12 +91,13 @@ class EmptyObject {
   external factory EmptyObject();
 }
 
+/// **Deprecated. use [js_util.jsify]/[jsifyAndAllowInterop] based on your needs.**
+///
 /// Returns [map] converted to a JavaScript object, similar to
 /// `new JsObject.jsify` in `dart:js`.
 ///
 /// Recursively converts nested [Map]s, and wraps [Function]s with [allowInterop].
-///
-/// TODO: deprecate and switch over to using [js_util.jsify]
+@Deprecated('4.0.0')
 dynamic jsify(Map map) {
   var jsMap = js_util.newObject();
 
@@ -111,4 +112,65 @@ dynamic jsify(Map map) {
   });
 
   return jsMap;
+}
+
+// The following code is adapted from `package:js` in the dart-lang/sdk repo:
+// https://github.com/dart-lang/sdk/blob/1.21.0/sdk/lib/js_util/dart2js/js_util_dart2js.dart#L25
+//
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// The same as [js_util.jsify], except [Function]s are converted with [allowInterop].
+///
+/// ---
+///
+/// *From [js_util.jsify]'s docs:*
+///
+/// WARNING: performance of this method is much worse than other util
+/// methods in this library. Only use this method as a last resort.
+///
+/// Recursively converts a JSON-like collection of Dart objects to a
+/// collection of JavaScript objects and returns a [JsObject] proxy to it.
+///
+/// [object] must be a [Map] or [Iterable], the contents of which are also
+/// converted. Maps and Iterables are copied to a new JavaScript object.
+/// Primitives and other transferable values are directly converted to their
+/// JavaScript type, and all other objects are proxied.
+dynamic jsifyAndAllowInterop(object) {
+  if (object is! Map && object is! Iterable) {
+    throw new ArgumentError.value(object, 'object', 'must be a Map or Iterable');
+  }
+  return _convertDataTree(object);
+}
+
+_convertDataTree(data) {
+  final _convertedObjects = new Map.identity();
+
+  _convert(o) {
+    if (_convertedObjects.containsKey(o)) {
+      return _convertedObjects[o];
+    }
+    if (o is Map) {
+      final convertedMap = js_util.newObject();
+      _convertedObjects[o] = convertedMap;
+      for (var key in o.keys) {
+        js_util.setProperty(convertedMap, key, _convert(o[key]));
+      }
+      return convertedMap;
+    } else if (o is Iterable) {
+      final convertedList = [];
+      _convertedObjects[o] = convertedList;
+      convertedList.addAll(o.map(_convert));
+      return convertedList;
+    } else if (o is Function) {
+      final convertedFunction = allowInterop(o);
+      _convertedObjects[o] = convertedFunction;
+      return convertedFunction;
+    } else {
+      return o;
+    }
+  }
+
+  return _convert(data);
 }
