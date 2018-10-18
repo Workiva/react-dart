96116b5c05d3cebb34838a5f6d6035d6dd23808f
Aaron Lademann <aaron.lademann@workiva.com>
2018-10-18 10:46:07 -0700

    Revert unnecessary breaking change

--- lib/react.dart
+++ lib/react.dart
@@ -667,7 +667,7 @@ class SyntheticWheelEvent extends SyntheticEvent {
 }
 
 /// Registers [componentFactory] on both client and server.
-ComponentRegistrar registerComponent = (ComponentFactory componentFactory, [Iterable<String> skipMethods]) {
+/*ComponentRegistrar*/Function registerComponent = (/*ComponentFactory*/componentFactory, [/*Iterable<String>*/ skipMethods]) {
   throw new Exception('setClientConfiguration must be called before registerComponent.');
 };
 
