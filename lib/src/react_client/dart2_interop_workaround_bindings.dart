fe3db667f48dfa55eee341ab43bfad95f46b5ac2
Aaron Lademann <aaron.lademann@workiva.com>
2018-10-18 16:12:07 -0700

    Work around dart2js binding bug
    + Issue where a JS interop class cannot have the same name as a JS class
    + TODO: Create SDK issue

---
+++ lib/src/react_client/dart2_interop_workaround_bindings.dart
@@ -0,0 +1,14 @@
+@JS()
+library react_client.src.dart2_interop_workaround_bindings;
+
+import 'dart:html';
+
+import 'package:js/js.dart';
+import 'package:react/react_client/react_interop.dart';
+
+@JS()
+abstract class ReactDOM {
+  external static Element findDOMNode(object);
+  external static ReactComponent render(ReactElement component, Element element);
+  external static bool unmountComponentAtNode(Element element);
+}
