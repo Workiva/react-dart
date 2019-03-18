2837ca7b4af4becea5ee5c047b80d26cfaf40aac
Joseph Bingham <joe.bingham@workiva.com>
2019-03-18 14:12:03 -0700

    Un-deprecate shouldComponentUpdateWithContext and restore old doc comments

--- lib/react.dart
+++ lib/react.dart
@@ -328,14 +328,15 @@ abstract class Component {
   /// See: <https://facebook.github.io/react/docs/react-component.html#updating-shouldcomponentupdate>
   bool shouldComponentUpdate(Map nextProps, Map nextState) => true;
 
-  /// > __DEPRECATED - DO NOT USE__
-  /// >
-  /// > This API was never stable in any version of ReactJS, and was replaced with a new, incompatible context API
-  /// > in ReactJS 16 that will be exposed in version `5.0.0` of the `react` Dart package via a
-  /// > new version of `Component` called `Component2`.
-  /// >
-  /// > This will be completely removed when the JS side of it is slated for removal (ReactJS 17 / react.dart 6.0.0)
-  @Deprecated('6.0.0')
+  /// ReactJS lifecycle method that is invoked before rendering when [nextProps], [nextState], or [nextContext] are
+  /// being received.
+  /// Use this as an opportunity to return `false` when you're certain that the transition to the new props, state, and
+  /// context will not require a component update.
+  ///
+  /// __Note__: This method is called before [shouldComponentUpdate]. Returning `null` will defer the update to the
+  /// result of [shouldComponentUpdate], but [shouldComponentUpdate] is not called if a valid `bool` is returned.
+  ///
+  /// See: <https://facebook.github.io/react/docs/react-component.html#updating-shouldcomponentupdate>
   bool shouldComponentUpdateWithContext(
           Map nextProps, Map nextState, Map nextContext) =>
       null;
