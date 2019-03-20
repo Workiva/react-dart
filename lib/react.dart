5a5d9693e5e636edf163cacc01d8a3fbc1017341
Joseph Bingham <joe.bingham@workiva.com>
2019-03-20 09:59:02 -0700

    Re-Add deprecation message

--- lib/react.dart
+++ lib/react.dart
@@ -327,15 +327,15 @@ abstract class Component {
   /// See: <https://facebook.github.io/react/docs/react-component.html#updating-shouldcomponentupdate>
   bool shouldComponentUpdate(Map nextProps, Map nextState) => true;
 
-  /// ReactJS lifecycle method that is invoked before rendering when [nextProps], [nextState], or [nextContext] are
-  /// being received.
-  /// Use this as an opportunity to return `false` when you're certain that the transition to the new props, state, and
-  /// context will not require a component update.
+  /// > __DEPRECATED - DO NOT USE__
   ///
-  /// __Note__: This method is called before [shouldComponentUpdate]. Returning `null` will defer the update to the
-  /// result of [shouldComponentUpdate], but [shouldComponentUpdate] is not called if a valid `bool` is returned.
+  /// > This API was never stable in any version of ReactJS, and was replaced with a new, incompatible context API
+  /// > being received.
+  /// > in ReactJS 16 that will be exposed in version `5.0.0` of the `react` Dart package via a
+  /// > new version of `Component` called `Component2`.
   ///
-  /// See: <https://facebook.github.io/react/docs/react-component.html#updating-shouldcomponentupdate>
+  /// > This will be completely removed when the JS side of it is slated for removal (ReactJS 17 / react.dart 6.0.0)
+  @Deprecated('6.0.0')
   bool shouldComponentUpdateWithContext(Map nextProps, Map nextState, Map nextContext) => null;
 
   /// ReactJS lifecycle method that is invoked immediately before rendering when [nextProps] or [nextState] are being
