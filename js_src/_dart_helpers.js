43ba027f489025ab610933f91bb17fa93a79b48d
Keal Jones <keal.jones@workiva.com>
2019-05-17 11:41:35 -0700

    reset to 5.0.0 version and rerun build

--- js_src/_dart_helpers.js
+++ js_src/_dart_helpers.js
@@ -1,7 +1,12 @@
 /**
  * react-dart JS interop helpers (used by react_client.dart and react_client/js_interop_helpers.dart)
  */
-
+/// A JS side function to allow Dart to throw an error from JS in order to catch it Dart side.
+/// Used within Component2 error boundry methods to dartify the error argument.
+/// See: https://github.com/dart-lang/sdk/issues/36363
+function _throwErrorFromJS(error) {
+  throw error;
+}
 function _createReactDartComponentClass(dartInteropStatics, componentStatics, jsConfig) {
   class ReactDartComponent extends React.Component {
     constructor(props, context) {
@@ -12,26 +17,26 @@ function _createReactDartComponentClass(dartInteropStatics, componentStatics, js
       dartInteropStatics.handleComponentWillMount(this.dartComponent);
     }
     componentDidMount() {
-      dartInteropStatics.handleComponentDidMount(this.dartComponent);
-    }
-    /*
-    /// This cannot be used with UNSAFE_ lifecycle methods.
-    getDerivedStateFromProps(nextProps, prevState) {
-      return dartInteropStatics.handleGetDerivedStateFromProps(this.props.internal, nextProps.internal);
-    }
-    */
+        dartInteropStatics.handleComponentDidMount(this.dartComponent);
+      }
+      /*
+      /// This cannot be used with UNSAFE_ lifecycle methods.
+      getDerivedStateFromProps(nextProps, prevState) {
+        return dartInteropStatics.handleGetDerivedStateFromProps(this.props.internal, nextProps.internal);
+      }
+      */
     UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
       dartInteropStatics.handleComponentWillReceiveProps(this.dartComponent, nextProps.internal, nextContext);
     }
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-      return dartInteropStatics.handleShouldComponentUpdate(this.dartComponent, nextContext);
-    }
-    /*
-    /// This cannot be used with UNSAFE_ lifecycle methods.
-    getSnapshotBeforeUpdate() {
-      return dartInteropStatics.handleGetSnapshotBeforeUpdate(this.props.internal, prevProps.internal);
-    }
-    */
+        return dartInteropStatics.handleShouldComponentUpdate(this.dartComponent, nextContext);
+      }
+      /*
+      /// This cannot be used with UNSAFE_ lifecycle methods.
+      getSnapshotBeforeUpdate() {
+        return dartInteropStatics.handleGetSnapshotBeforeUpdate(this.props.internal, prevProps.internal);
+      }
+      */
     UNSAFE_componentWillUpdate(nextProps, nextState, nextContext) {
       dartInteropStatics.handleComponentWillUpdate(this.dartComponent, nextContext);
     }
@@ -47,12 +52,10 @@ function _createReactDartComponentClass(dartInteropStatics, componentStatics, js
       return result;
     }
   }
-
   // React limits the accessible context entries
   // to the keys specified in childContextTypes/contextTypes.
   var childContextKeys = jsConfig && jsConfig.childContextKeys;
   var contextKeys = jsConfig && jsConfig.contextKeys;
-
   if (childContextKeys && childContextKeys.length !== 0) {
     ReactDartComponent.childContextTypes = {};
     for (var i = 0; i < childContextKeys.length; i++) {
@@ -60,26 +63,22 @@ function _createReactDartComponentClass(dartInteropStatics, componentStatics, js
     }
     // Only declare this when `childContextKeys` is non-empty to avoid unnecessarily
     // creating interop context objects for components that won't use it.
-    ReactDartComponent.prototype['getChildContext'] = function() {
+    ReactDartComponent.prototype['getChildContext'] = function () {
       return dartInteropStatics.handleGetChildContext(this.dartComponent);
     };
   }
-
   if (contextKeys && contextKeys.length !== 0) {
     ReactDartComponent.contextTypes = {};
     for (var i = 0; i < contextKeys.length; i++) {
       ReactDartComponent.contextTypes[contextKeys[i]] = React.PropTypes.object;
     }
   }
-
   return ReactDartComponent;
 }
-
 function _markChildValidated(child) {
   const store = child._store;
   if (store) store.validated = true;
 }
-
 module.exports = {
   _createReactDartComponentClass,
   _markChildValidated,
