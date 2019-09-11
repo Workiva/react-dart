fd4606d27e687ae86b09f9e421dbaa565e95251f
Greg Littlefield <greg.littlefield@workiva.com>
2019-09-10 17:23:12 -0700

    Add custom bridge tests

--- test/react_client/bridge_test.dart
+++ test/react_client/bridge_test.dart
@@ -13,11 +13,28 @@ main() {
 
   // Test behavior that isn't functionally tested via component lifecycle tests.
   group('Component2Bridge', () {
-    test(
-        '.forComponent returns the bridge used by that mounted component,'
-        ' which by default is a const instance of Component2BridgeImpl', () {
-      final instance = getDartComponent<_Foo>(render(Foo({})));
-      expect(Component2Bridge.forComponent(instance), same(const Component2BridgeImpl()));
+    group('.forComponent returns the bridge used by that mounted component, which', () {
+      test('by default is a const instance of Component2BridgeImpl', () {
+        final instance = getDartComponent<_Foo>(render(Foo({})));
+        expect(Component2Bridge.forComponent(instance), same(const Component2BridgeImpl()));
+      });
+
+      test('can be a custom bridge specified via registerComponent2', () {
+        customBridgeCalls.clear();
+        final instance = getDartComponent<_BridgeTest>(render(BridgeTest({})));
+        final bridge = Component2Bridge.forComponent(instance);
+        expect(bridge, isNotNull);
+        expect(
+          customBridgeCalls,
+          // Use `contains` since the static component instance and its bridge also get instantiated,
+          // adding entries to customBridgeCalls.
+          // `equals` is needed otherwise deep matching with matchers won't work
+          contains(equals({
+            'component': same(instance),
+            'returnedBridge': same(bridge),
+          })),
+        );
+      });
     });
 
     group('- Component2BridgeImpl', () {
@@ -34,3 +51,21 @@ class _Foo extends react.Component2 {
   @override
   render() => react.div({});
 }
+
+final customBridgeCalls = [];
+
+final BridgeTest = react.registerComponent2(() => new _BridgeTest(), bridgeFactory: (component) {
+  final returnedBridge = new CustomComponent2Bridge();
+  customBridgeCalls.add({
+    'component': component,
+    'returnedBridge': returnedBridge,
+  });
+  return returnedBridge;
+});
+
+class _BridgeTest extends react.Component2 {
+  @override
+  render() => react.div({});
+}
+
+class CustomComponent2Bridge extends Component2BridgeImpl {}
