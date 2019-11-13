3242857faf01ba51834a8a7a1521253630e9b9c9
Keal Jones <keal.jones@workiva.com>
2019-11-13 12:08:09 -0700

    remove periods because linux

--- test/factory/dart_function_factory_test.dart
+++ test/factory/dart_function_factory_test.dart
@@ -29,7 +29,7 @@ main() {
           expect(_getJsFunctionName(FunctionFoo.reactFunction), '_FunctionFoo');
 
           expect(FunctionFoo.displayName, _getJsFunctionName(FunctionFoo.reactFunction));
-        }, tags: ['fails-on-2.4.1']);
+        }, tags: ['fails-on-241']);
 
         test('is populated by the provided argument', () {
           expect(NamedFunctionFoo.displayName, 'Bar');
