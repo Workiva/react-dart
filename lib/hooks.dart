71b57eae79b6124baad0c443a1734d41e1066ffa
Sydney Jodon <sydney.jodon@workiva.com>
2019-11-18 14:40:14 -0700

    Add tests

--- lib/hooks.dart
+++ lib/hooks.dart
@@ -102,6 +102,4 @@ StateHook<T> useState<T>(T initialValue) => StateHook(initialValue);
 /// Learn more: <https://reactjs.org/docs/hooks-reference.html#lazy-initial-state>.
 StateHook<T> useStateLazy<T>(T init()) => StateHook.lazy(init);
 
-Function useCallback(Function callback, List dependencies) {
-  return React.useCallback(allowInterop(callback), dependencies);
-}
+Function useCallback(Function callback, List dependencies) => React.useCallback(allowInterop(callback), dependencies);
