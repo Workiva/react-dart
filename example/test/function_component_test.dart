17fdb154470e32d3d0f550c49f3298eb386300b4
Sydney Jodon <sydney.jodon@workiva.com>
2020-01-06 11:27:12 -0700

    Clean up diff

--- example/test/function_component_test.dart
+++ example/test/function_component_test.dart
@@ -27,7 +27,7 @@ HookTestComponent(Map props) {
   }, [count.value]);
 
   return react.div({}, [
-    react.button({'onClick': (_) => count.set(0), 'key': 'ust1'}, ['Reset']),
+    react.button({'onClick': (_) => count.set(1), 'key': 'ust1'}, ['Reset']),
     react.button({'onClick': (_) => count.setWithUpdater((prev) => prev + 1), 'key': 'ust2'}, ['+']),
     react.br({'key': 'ust3'}),
     react.p({'key': 'ust4'}, ['${count.value} is ${evenOdd.value}']),
