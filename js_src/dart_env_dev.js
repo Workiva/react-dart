82da37374f0614faa34a0d9c331df59844042d61
Corwin Sheahan <corwin.sheahan@workiva.com>
2020-10-05 13:40:37 -0600

    Add chrome 86 workaround

--- js_src/dart_env_dev.js
+++ js_src/dart_env_dev.js
@@ -1,1 +1,8 @@
 React.__isDevelopment = true;
+
+if (typeof window.MemoryInfo == "undefined") {
+  if (typeof window.performance.memory != "undefined") {
+    window.MemoryInfo = function () {};
+    window.MemoryInfo.prototype = window.performance.memory.__proto__;
+  }
+}
