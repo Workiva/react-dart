25718c78b6773880d9c9b61629e946dfdd66996d
Joe Bingham <joe.bingham@workiva.com>
2020-11-06 12:10:55 -0700

    Move export

--- lib/react_client.dart
+++ lib/react_client.dart
@@ -21,7 +21,6 @@ export 'package:react/react_client/component_factory.dart'
         JsBackedMapComponentFactoryMixin;
 export 'package:react/react_client/zone.dart' show componentZone;
 export 'package:react/src/react_client/chain_refs.dart' show chainRefs, chainRefList;
-export 'package:react/src/react_client/event_helpers.dart';
 export 'package:react/src/react_client/event_factory.dart'
     show
         syntheticEventFactory,
