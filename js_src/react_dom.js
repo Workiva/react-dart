257c6df427cf9bd3a44c70a909e484820037c793
joebingham-wk <46691367+joebingham-wk@users.noreply.github.com>
2022-04-08 09:44:41 -0700

    This works with coverage

--- js_src/react_dom.js
+++ js_src/react_dom.js
@@ -1,9 +1,9 @@
-const ReactRedux = require('react-redux');
+// const ReactRedux = require('react-redux');
 const ReactDOM = require('react-dom');
-
-window.ReactDOM = ReactDOM;
-window.ReactRedux = ReactRedux;
-
-if (process.env.NODE_ENV == 'development') {
-    require('./react_dom_dev');
-}
+//
+// window.ReactDOM = ReactDOM;
+// window.ReactRedux = ReactRedux;
+//
+// if (process.env.NODE_ENV == 'development') {
+//     require('./react_dom_dev');
+// }
