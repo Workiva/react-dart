9065657a1af26f47284034642a0641df0ea618c8
Keal Jones <keal.jones@workiva.com>
2023-04-19 11:54:33 -0700

    add suspense tests   - temporary lazy impl

---
+++ example/lazy/lazy.dart
@@ -0,0 +1,55 @@
+@JS()
+library js_components;
+
+import 'dart:html';
+import 'dart:js_util';
+
+import 'package:js/js.dart';
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart';
+import 'package:react/react_client/react_interop.dart';
+import 'package:react/react_dom.dart' as react_dom;
+import 'package:react/src/js_interop_util.dart';
+import './simple_component.dart' deferred as simple;
+
+
+@JS('React.lazy')
+external ReactClass jsLazy(Promise Function() factory);
+
+// Only intended for testing purposes, Please do not copy/paste this into repo.
+// This will most likely be added to the PUBLIC api in the future,
+// but needs more testing and Typing decisions to be made first.
+ReactJsComponentFactoryProxy lazy(Future<ReactComponentFactoryProxy> factory()) => ReactJsComponentFactoryProxy(
+      jsLazy(
+        allowInterop(
+          () => futureToPromise(
+            // React.lazy only supports "default exports" from a module.
+            // This `{default: yourExport}` workaround can be found in the React.lazy RFC comments.
+            // See: https://github.com/reactjs/rfcs/pull/64#issuecomment-431507924
+            (() async => jsify({'default': (await factory()).type}))(),
+          ),
+        ),
+      ),
+    );
+
+main() {
+  var content = wrapper({});
+
+  react_dom.render(content, querySelector('#content'));
+}
+
+final lazyComponent = lazy(() async {
+  await simple.loadLibrary();
+  await Future.delayed(Duration(seconds: 5));
+  return simple.SimpleComponent;
+});
+
+var wrapper = react.registerFunctionComponent(WrapperComponent, displayName: 'wrapper');
+
+WrapperComponent(Map props) {
+  return react.div({
+    'id': 'lazy-wrapper'
+  }, [
+    react.Suspense({'fallback': 'Loading...'}, [lazyComponent({})])
+  ]);
+}
--- lib/src/js_interop_util.dart
+++ lib/src/js_interop_util.dart
@@ -19,3 +19,21 @@ class JsPropertyDescriptor {
 external void defineProperty(dynamic object, String propertyName, JsPropertyDescriptor descriptor);
 
 String getJsFunctionName(Function object) => getProperty(object, 'name') ?? getProperty(object, '\$static_name');
+
+/// Creates JS `Promise` which is resolved when [future] completes.
+///
+/// See also:
+/// - [promiseToFuture]
+Promise futureToPromise<T>(Future<T> future) {
+  return Promise(allowInterop((Function resolve, Function reject) {
+    future.then((result) => resolve(result), onError: reject);
+  }));
+}
+
+@JS()
+abstract class Promise {
+  external factory Promise(
+      Function(dynamic Function(dynamic value) resolve, dynamic Function(dynamic error) reject) executor);
+
+  external Promise then(dynamic Function(dynamic value) onFulfilled, [dynamic Function(dynamic error) onRejected]);
+}
