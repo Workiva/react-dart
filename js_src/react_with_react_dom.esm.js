fdfee6c263d6fe63ba6a7ca0f8cfa09e3f81eddb
Matthew Nitschke <matthew.nitschke@workiva.com>
2023-06-15 08:30:26 -0500

    added specific esm output version of react

---
+++ js_src/react_with_react_dom.esm.js
@@ -0,0 +1,4 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+export { React, ReactDom }
--- tool/tsup.config.js
+++ tool/tsup.config.js
@@ -3,13 +3,11 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   format: ['esm'],
   target: 'es5',
-  outExtension({ format }) {
-    return {
-      js: format === 'esm' ? '.esm.js' : '.js',
-    };
+  outExtension() {
+    return { js: '.js' };
   },
   outDir: './lib',
-  entry: ['js_src/react_with_react_dom.js'],
+  entry: ['js_src/react_with_react_dom.esm.js'],
   noExternal: [/.*/],
   platform: 'browser',
   splitting: false,
